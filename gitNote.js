eed1a4a77cd3474387915a1d97e3a2dfc684e07b
funlee <i@funlee.cn>
2018-03-15 16:49:21 +0800

    git reset

---
+++ gitNote.js
@@ -0,0 +1,23 @@
+/*
+
+git add learn.js  添加到暂存区
+
+git commit -m 'learnning git'  把暂存区的所有内容提交到当前分支，默认 master
+
+git status
+
+git diff
+
+git diff HEAD -- some.js
+
+git log
+
+git reset --hard HEAD^  HEAD 指向的版本就是当前版本
+
+git reset --hard commit_id
+
+git reflog  查看命令历史
+
+
+
+*/
--- learn.js
+++ learn.js
@@ -9,3 +9,6 @@ const d = 'git status'
 const e = 'diff'
 
 const f = 'back'
+
+const str = '需要提交的文件修改通通放到暂存区，然后，一次性提交暂存区的所有修改'
+
